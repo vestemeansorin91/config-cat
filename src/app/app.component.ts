54c014bf0e2ad86da5a565ed7ea095b7ffe57d37
Sorin Vestemean <vestemeansorin91@gmail.com>
2021-11-16 14:46:18 +0200

    implemented feature toggles, environments, identifier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import * as configcat from "configcat-js";
 
+import { Observable, from } from "rxjs";
+
+import { Component } from '@angular/core';
+import { IConfigCatClient } from 'configcat-common';
+import { environment } from './../environments/environment';
+
+const PROD_KEY = 'OqjZCL4tFkGE81nOqZJAOA/wOtIC1UAt0Kr6vRZpSfeBA';
+const TEST_KEY = 'OqjZCL4tFkGE81nOqZJAOA/B8v7Zcm71k2f3UlX6OLpZQ';
+
+interface SettingToggle {
+  settingKey: string;
+  settingValue: boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'config-cat';
+  public configCatClient!: IConfigCatClient;
+  public allToggles$!: Observable<SettingToggle[]>;
+
+  // ENVIRONMENTS
+  public prodKey = PROD_KEY;
+  public testKey = TEST_KEY;
+  public sdkKey = PROD_KEY;
+  
+  public identifier = '';
+
+
+  constructor() {
+    this.initConfigAndToggles();
+  }
+
+  public identifierChanged(identifier: string) {
+    this.allToggles$ = from(this.configCatClient.getAllValuesAsync({ identifier }));
+  }
+
+  public environmentChanged(environment: string) {
+    this.initConfigAndToggles();
+  }
+
+  public typeOfToggle(toggle: boolean | number) {
+    return typeof toggle;
+  }
+
+
+  private initConfigAndToggles() {
+    this.configCatClient = configcat.createClientWithAutoPoll(this.sdkKey, { pollIntervalSeconds: 2 });
+    this.allToggles$ = from(this.configCatClient.getAllValuesAsync({ identifier: this.identifier }));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    // ANGULAR MATERIAL MODULES
+    MatSlideToggleModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
